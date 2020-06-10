84e1f15808813438e83ab50d708ab41c936023e5
Thiago Alonso <thiago.alonso@avenuecode.com>
2020-06-10 10:38:31 -0300

    adds base structure for gitlab script

---
+++ scripts/gitlab.py
@@ -0,0 +1,14 @@
+import requests
+
+if __name__ == '__main__':
+
+  # authenticate
+  
+
+  # create new branch from master
+
+
+  # update file from template + env variables
+
+
+  # create and submit merge request
